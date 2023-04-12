059886cd8e6b4e2f3bb52593337f9182fa1eefce
msi43089 <msi43089@gmail.com>
2023-04-12 13:55:16 -0400

    reformat services

--- src/Book.js
+++ src/Book.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 
 export default function Book() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://www.vagaro.com//resources/WidgetEmbeddedLoader/OZqoDp4rCp4cT3qnV3avV34mC2PeFJ4mC30m9cyUeJUtjP0dDxkJEvwRapWUep2sERAJDXwPapcUbfY?v=fsPyai6oJ0FqLvryzkXT0ERjhN9XJYX3RGeXiZgyznfu#";
+    script.async = true;
+
+    const vagaroParent = document.querySelector(".vagaro");
+    vagaroParent.appendChild(script);
+    return () => {
+      vagaroParent.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div>
-      <div id="frameTitle" className="embedded-widget-title"></div>
-      <div className="vagaro">
-        <a href="https://www.vagaro.com/pro/">Powered by Vagaro</a>&nbsp;
-        <a href="https://www.vagaro.com/pro/salon-software">Salon Software</a>
-        ,&nbsp;
-        <a href="https://www.vagaro.com/pro/spa-software">Spa Software</a>
-        &nbsp;&amp;&nbsp;
-        <a href="https://www.vagaro.com/pro/fitness-software">
-          Fitness Software
-        </a>
-        <Helmet>
-          <script
-            type="text/javascript"
-            src="https://www.vagaro.com//resources/WidgetEmbeddedLoader/OZqoDp4rCp4cT3qmV35y6RuSdBuOc1WJD1wOc1WO61CxdfkJE1wgEJgoapOUcPCvdfQOW?v=mIvwzavaOdz1FNIyQRzhNz9SlgoDjTBY13UyaiGiv6Ry#"
-          ></script>
-        </Helmet>
-      </div>
+    <div className="book">
+      <div className="vagaro"></div>
     </div>
   );
 }
--- src/Services.js
+++ src/Services.js
@@ -2,51 +2,51 @@ export default function Services() {
   return (
     <div className="services">
       <h2 className="about-title">Services</h2>
-      <div className="about-body">
+      <div className="hc-body">
         <div className="service-title">Hair Color</div>
         <div className="hc-left">
           <table>
             <tbody>
               <tr>
                 <td>Platinum Card:</td>
-                <td>$385</td>
+                <td class="price">$385</td>
               </tr>
               <br />
               <tr>
                 <td>Heavy Full Foil:</td>
-                <td>$300</td>
+                <td class="price">$300</td>
               </tr>
               <tr>
                 <td>Classic Full Foil:</td>
-                <td>$230</td>
+                <td class="price">$230</td>
               </tr>
               <br />
               <tr>
                 <td>Heavy Partial:</td>
-                <td>$250</td>
+                <td class="price">$250</td>
               </tr>
               <tr>
                 <td>Classic Partial:</td>
-                <td>$185</td>
+                <td class="price">$185</td>
               </tr>
               <br />
               <tr>
                 <td>Heavy Balayage:</td>
-                <td>$300</td>
+                <td class="price">$300</td>
               </tr>
               <tr>
                 <td>Classic Balayage:</td>
-                <td>$265</td>
+                <td class="price">$265</td>
               </tr>
               <br />
               <tr>
                 <td>Face Frame Foil:</td>
-                <td>$140</td>
+                <td class="price">$140</td>
               </tr>
               <br />
               <tr>
                 <td>Color Corrections:</td>
-                <td>$350+</td>
+                <td class="price">$350+</td>
               </tr>
             </tbody>
           </table>
@@ -56,45 +56,66 @@ export default function Services() {
             <tbody>
               <tr>
                 <td>Root Touch Up:</td>
-                <td>$75</td>
+                <td class="price">$75</td>
               </tr>
               <tr>
                 <td>Single Process:</td>
-                <td>$130</td>
+                <td class="price">$130</td>
               </tr>
               <tr>
                 <td>Bleach & Tone Touch Up:</td>
-                <td>$175</td>
+                <td class="price">$175</td>
               </tr>
               <br />
               <tr>
                 <td>Toner:</td>
-                <td>$20</td>
+                <td class="price">$20</td>
               </tr>
               <tr>
                 <td>Root Tap:</td>
-                <td>$20</td>
+                <td class="price">$20</td>
               </tr>
               <tr>
                 <td>Extra Color:</td>
-                <td>$22</td>
+                <td class="price">$22</td>
               </tr>
               <tr>
                 <td>Low lights:</td>
-                <td>$40</td>
+                <td class="price">$40</td>
               </tr>
               <tr>
                 <td>Glaze:</td>
-                <td>$65</td>
+                <td class="price">$65</td>
               </tr>
               <br />
               <tr>
                 <td>Haircut:</td>
-                <td>$50</td>
+                <td class="price">$50</td>
               </tr>
               <tr>
                 <td>Haircut w/ color:</td>
-                <td>$40</td>
+                <td class="price">$40</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="styling-body">
+        <div className="styling-title">Styling</div>
+        <div className="styling">
+          <table>
+            <tbody>
+              <tr>
+                <td>Blow out:</td>
+                <td className="price">$40</td>
+              </tr>
+              <tr>
+                <td>Blow out + iron working:</td>
+                <td className="price">$60</td>
+              </tr>
+              <tr>
+                <td>Updo:</td>
+                <td className="price">$90+</td>
               </tr>
             </tbody>
           </table>
