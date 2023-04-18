c6566f40013b405f495e3436355683459ea51e8b
msi43089 <msi43089@gmail.com>
2023-04-17 22:44:46 -0400

    Added Link to Home Div

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <>
       <div className="home">
+        <Link to="/book" >
         <div className="centered">RESERVE HAIR THERAPY HERE</div>
+        </Link>
       </div>
     </>
   );
