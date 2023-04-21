74f6e527370c11183fbf3d7bdf58f8c8b791196b
msi43089 <msi43089@gmail.com>
2023-04-20 21:15:29 -0400

    Finished Bronde def

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,28 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div className="home">
+      <div className="home"  >
+        <div className="home-bg">
         <div className="home-title">BRONDE SOCIETY HAIR CO.</div>
         <div className="home-slogan">life is better bronde</div>
         <Link to="/book">
           <div className="centered">RESERVE HAIR THERAPY HERE</div>
         </Link>
-        {/* <div className="home-container">
+        </div>
+        <div className="home-container">
           <div className="bronde-def">
+          <div className="bronde-img">
+              <img src="/images/bronde-img-small.webp" alt="Bronde Hair" />
+            </div>
             <div className="def">
               <h2>Bronde</h2>
               <p>/brand/</p>
               <p>adjective</p>
-              <p>adjective: bronde; adjective: brone</p>
+              <p>adjective: bronde; adjective: brond</p>
               <br/>
               <p>A color technique that fuses brown and blonde, to create a flattering, sun-kissed color result.</p>
             </div>
-            <div>
-
-            </div>
+           
           </div>
           <div className="about-salon">
 
@@ -29,7 +32,7 @@ export default function Home() {
           <div className="reviews">
 
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
