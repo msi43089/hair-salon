2ae98045c2902b509898734698c7125224da568b
msi43089 <msi43089@gmail.com>
2023-05-23 18:50:22 -0400

    Added Gift Card Tab

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ export default function Book() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://www.vagaro.com//resources/WidgetEmbeddedLoader/OZqoDp4rCp4cT3qnV3avV34mC2PeFJ4mC30m9cyUeJUtjP0dDxkJEvwRapWUep2sERAJDXwPapcUbfY?v=fsPyai6oJ0FqLvryzkXT0ERjhN9XJYX3RGeXiZgyznfu#";
+      "https://www.vagaro.com//resources/WidgetEmbeddedLoader/OZqoDp4rCp4cT3qnV39y79oz34mC2PeFJ4mC30m9cyUeJUtjP0dDxkJEvwRapWUep2sERAJDXwPapcUbfY?v=fsPyai6oJ0FqLvryzkXT0ERjhN9XJYX3RGeXiZgyznfu#";
     script.async = true;
 
     const vagaroParent = document.querySelector(".vagaro");
